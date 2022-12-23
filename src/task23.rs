9539082b8cc3ae397f21db697eb6f5179a7beb06
STerliakov <terlya.stas@gmail.com>
2022-12-23 12:46:52 +0300

    Simplify code, use grid - 150ms for 2nd part

--- src/task23.rs
+++ src/task23.rs
@@ -1,10 +1,5 @@
+use grid::Grid;
 use itertools::Itertools;
-use std::ops::BitAnd;
-
-const N: (isize, isize) = (-1, 0);
-const S: (isize, isize) = (1, 0);
-const W: (isize, isize) = (0, -1);
-const E: (isize, isize) = (0, 1);
 
 #[derive(Clone, Debug, Default)]
 struct Cell {
@@ -12,162 +7,140 @@ struct Cell {
     wanted: Vec<(usize, usize)>,
 }
 
-fn parse_input(inp: &str) -> Vec<Vec<Cell>> {
-    inp.lines()
-        .map(|row| {
-            row.bytes()
-                .map(|b| Cell {
+fn parse_input(inp: &str) -> Grid<Cell> {
+    let mut len = 0;
+    Grid::from_vec(
+        inp.lines()
+            .flat_map(|row| {
+                len = row.len();
+                row.bytes().map(|b| Cell {
                     wanted: vec![],
                     occupied: b == b'#',
                 })
-                .collect()
-        })
-        .collect()
+            })
+            .collect(),
+        len,
+    )
 }
 
-fn get_actual_width(map: &Vec<Vec<Cell>>) -> (usize, usize) {
-    let w = map[0].len() - 1;
-
-    let mut left = usize::MAX;
-    for row in map {
-        left = left.min(row.iter().position(|c| c.occupied).unwrap_or(usize::MAX));
-        if left == 0 {
-            break;
-        }
-    }
-    let mut right = 0;
-    for row in map {
-        right = right.max(row.iter().rposition(|c| c.occupied).unwrap_or(0));
-        if right == w {
-            break;
-        }
-    }
-    (left, right)
-}
-
-fn solve(map: &mut Vec<Vec<Cell>>, max_steps: Option<u32>) -> u32 {
-    let mut directions = [N, S, W, E];
+fn solve(map: &mut Grid<Cell>, max_steps: Option<u32>) -> u32 {
+    let mut directions = [
+        (-1, 0), // N
+        (1, 0),  // S
+        (0, -1), // W
+        (0, 1),  // E
+    ];
+    let neighbours: Vec<_> = (-1..=1)
+        .cartesian_product(-1..=1)
+        .filter(|&(c, r)| c != 0 || r != 0)
+        .collect();
 
     let mut step = 0;
     let mut states = hashbrown::HashSet::<[(isize, isize); 4]>::new();
     loop {
         // Prepare
-        let (h, w) = (map.len(), map[0].len());
-        let (left, right) = get_actual_width(map);
-        if left == 0 {
-            map.iter_mut()
-                .for_each(|row| row.insert(0, Cell::default()));
+        let (h, w) = map.size();
+        if map.iter_col(w - 1).any(|c| c.occupied) {
+            map.push_col(vec![Cell::default(); h]);
         }
-        if right == w - 1 {
-            map.iter_mut().for_each(|row| row.push(Cell::default()));
+        if map.iter_col(0).any(|c| c.occupied) {
+            map.insert_col(0, vec![Cell::default(); h]);
         }
-        if map[0].iter().any(|c| c.occupied) {
-            map.insert(0, vec![Cell::default(); map[0].len()]);
+        if map.iter_row(h - 1).any(|c| c.occupied) {
+            map.push_row(vec![Cell::default(); map.cols()]);
         }
-        if map[h - 1].iter().any(|c| c.occupied) {
-            map.push(vec![Cell::default(); map[0].len()]);
+        if map.iter_row(0).any(|c| c.occupied) {
+            map.insert_row(0, vec![Cell::default(); map.cols()]);
         }
-        let (h, w) = (map.len(), map[0].len());
+        let (h, w) = map.size();
 
         // Step 1
-        let mut wanted = None;
-        for r in 1..h - 1 {
-            for c in 1..w - 1 {
-                if !map[r][c].occupied
-                    || (-1..=1)
-                        .cartesian_product(-1..=1)
-                        .map(|(dc, dr)| {
-                            dr == 0 && dc == 0
-                                || !map[(r as isize + dr) as usize][(c as isize + dc) as usize]
-                                    .occupied
-                        })
-                        .reduce(bool::bitand)
-                        .unwrap()
+        let mut wanted: Option<usize> = None;
+        for r in 1..h as isize - 1 {
+            for c in 1..w as isize - 1 {
+                if !map[r as usize][c as usize].occupied
+                    || neighbours
+                        .iter()
+                        .all(|(dc, dr)| !map[(r + dr) as usize][(c + dc) as usize].occupied)
                 {
                     continue;
                 }
                 for (i, &(dr, dc)) in directions.iter().enumerate() {
-                    let (new_r, new_c) = ((r as isize + dr) as usize, (c as isize + dc) as usize);
+                    let (new_r, new_c) = ((r + dr) as usize, (c + dc) as usize);
                     if dr == 0
-                        && (-1..=1)
-                            .map(|dr| map[(r as isize + dr) as usize][new_c].occupied)
-                            .all(|c| !c)
+                        && map
+                            .iter_col(new_c)
+                            .skip((r - 1) as usize)
+                            .take(3)
+                            .all(|cell| !cell.occupied)
                         || dc == 0
-                            && (-1..=1)
-                                .map(|dc| !map[new_r][(c as isize + dc) as usize].occupied)
-                                .reduce(bool::bitand)
-                                .unwrap()
+                            && map
+                                .iter_row(new_r)
+                                .skip((c - 1) as usize)
+                                .take(3)
+                                .all(|cell| !cell.occupied)
                     {
                         match wanted {
-                            None => wanted = Some(i),
                             Some(v) => wanted = Some(v.min(i)),
+                            None => wanted = Some(i),
                         }
-                        map[new_r][new_c].wanted.push((r, c));
+                        map[new_r][new_c].wanted.push((r as usize, c as usize));
                         break;
                     }
                 }
             }
         }
 
-        // Step 2
-        let mut moved = false;
-        for r in 0..h {
-            for c in 0..w {
-                if map[r][c].wanted.len() == 1 {
-                    map[r][c].occupied = true;
-                    let (old_r, old_c) = map[r][c].wanted[0];
-                    map[old_r][old_c].occupied = false;
-                    moved = true;
-                }
-                map[r][c].wanted.clear();
-            }
-        }
-
-        // Finalize
+        let mut has_moved = false;
         if let Some(idx) = wanted {
+            // Step 2
+            for r in 0..h {
+                for c in 0..w {
+                    let cell = &mut map[r][c];
+                    if cell.wanted.len() == 1 {
+                        cell.occupied = true;
+                        let (old_r, old_c) = cell.wanted[0];
+                        map[old_r][old_c].occupied = false;
+                        has_moved = true;
+                    }
+                    map[r][c].wanted.clear();
+                }
+            }
             directions[idx..].rotate_left(1);
         }
 
-        if moved {
+        // Finalize
+        if has_moved {
             states.clear();
         } else if !states.insert(directions) {
-            break;
+            break step;
         }
         step += 1;
         if max_steps.is_some() && step >= max_steps.unwrap() {
-            break;
+            break step;
         }
     }
-    step
 }
 
 pub fn prob1(inp: &str) -> usize {
     let mut map = parse_input(inp);
     solve(&mut map, Some(10));
 
-    // for row in &map {
-    //     println!(
-    //         "{}",
-    //         row.iter()
-    //             .map(|c| if c.occupied { "#" } else { "." })
-    //             .collect::<Vec<_>>()
-    //             .join("")
-    //     );
-    // }
-    // println!();
-
-    let (left, right) = get_actual_width(&map);
-    let top = map
-        .iter()
-        .position(|row| row.iter().any(|c| c.occupied))
+    let (h, w) = map.size();
+    let left = (0..w)
+        .find(|i| map.iter_col(*i).any(|c| c.occupied))
         .unwrap();
-    let bot = map
-        .iter()
-        .rposition(|row| row.iter().any(|c| c.occupied))
+    let right = (0..w)
+        .rfind(|i| map.iter_col(*i).any(|c| c.occupied))
         .unwrap();
-    map[top..=bot]
-        .iter()
-        .map(|row| row[left..=right].iter().filter(|c| !c.occupied).count())
+    let top = (0..h)
+        .find(|i| map.iter_row(*i).any(|c| c.occupied))
+        .unwrap();
+    let bot = (0..h)
+        .rfind(|i| map.iter_row(*i).any(|c| c.occupied))
+        .unwrap();
+    (top..=bot)
+        .map(|r| (left..=right).filter(|c| !map[r][*c].occupied).count())
         .sum()
 }
 
